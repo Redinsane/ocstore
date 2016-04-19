d699338d72a3c7b26310b83cd1e3dff83d917365
Redinsane <redinsane@ukr.net>
2016-04-19 17:58:08 +0300

    Delete init.php

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = "mysql:host=localhost;dbname=ads_new;charset=utf8";
-$opt = array(
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-);
-$pdo = new PDO($conn, 'root', '', $opt);
-$result = $pdo->query('SELECT * FROM ads');
